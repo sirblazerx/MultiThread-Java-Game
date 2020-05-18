5513ea8d2ea8897115c60a95b267fa02c5de9e77
sirblazerx <47962616+sirblazerx@users.noreply.github.com>
2020-05-18 15:09:55 +0800

    Add files via upload

---
+++ JavaGame.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagame;
+
+/**
+ *
+ * @author User
+ */
+
+import java.util.Random;
+
+
+public class JavaGame {
+
+    volatile int n = 0;
+    volatile int m = 0; //      (no of seconds) or max 20 attempt
+    volatile int t = 0;  // n > t
+     double point = 0.0;   //region =(1000.1000)
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
